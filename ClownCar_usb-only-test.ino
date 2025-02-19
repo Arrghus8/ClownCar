287e07a3dc9a2d3082fa1f7278b75d6bdddf0e1f
svirant <62872229+svirant@users.noreply.github.com>
2025-02-18 17:53:27 -0800

    Add files via upload

---
+++ ClownCar_usb-only-test.ino
@@ -0,0 +1,20 @@
+#include <EspUsbHostSerial_FTDI.h>// https://github.com/wakwak-koba/EspUsbHost in order to have FTDI support for the RT4K usb serial port, this is the easist method.
+                                  // Step 1 - Goto the github link above. Click the GREEN "<> Code" box and "Download ZIP"
+                                  // Step 2 - In Arudino IDE; goto "Sketch" -> "Include Library" -> "Add .ZIP Library"
+
+class SerialFTDI : public EspUsbHostSerial_FTDI {
+  void onNew() override {
+    submit((uint8_t *)"remote prof8\r", 13); // make sure to keep the \r at the end if changing the command
+  }
+};
+
+SerialFTDI usbDev;
+
+void setup() {
+  usbDev.begin(115200);
+}
+
+void loop()
+{
+  usbDev.task();
+}
